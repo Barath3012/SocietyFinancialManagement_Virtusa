b19aef7356b431d4da658d3e673db9db9342902b
sk2k3 <125984780+sk2k3@users.noreply.github.com>
2024-03-05 13:00:31 +0530

    display of list

--- SocietyFinance/src/app/app.component.ts
+++ SocietyFinance/src/app/app.component.ts
@@ -1,11 +1,24 @@
 // app.component.ts
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import fam from './data.json';
 
+interface Family{
+  id: string,
+  Name: string;
+  Members: number;
+  Rent: number;
+}
+console.log(fam);
 @Component({
   selector: 'app-root',
+  imports:[FormsModule,CommonModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   textInput: string = '';
+  fam: Family[] = fam;
 }
