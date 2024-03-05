35a2f0460ce452ab9dca4c8de9de008455729035
Saarah <saarahkhan2k3@gmail.com>
2024-03-05 09:37:03 +0530

    idk changes

--- SocietyFinance/src/app/app.component.spec.ts
+++ SocietyFinance/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'SocietyFinance' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SocietyFinance');
+    expect(app.title).toEqual('Saarah Ahmed');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SocietyFinance');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Saarah Ahmed');
   });
 });
