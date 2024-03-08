bf5a2c727ae9d834c78f32b52b73bba7f3fa088b
Saarah <saarahkhan2k3@gmail.com>
2024-03-08 14:02:33 +0530

    push this

--- SocietyFinance/src/app/app.component.ts
+++ SocietyFinance/src/app/app.component.ts
@@ -1,11 +1,24 @@
-// app.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  textInput: string = '';
+export class AppComponent implements OnInit {
+  rooms: any[];
+  carParking: any;
+  rent: any;
+  title: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('assets/data.json').subscribe(data => {
+      this.rooms = data.rooms;
+      this.carParking = data.car_parking;
+      this.rent = data.rent;
+    });
+  }
 }
